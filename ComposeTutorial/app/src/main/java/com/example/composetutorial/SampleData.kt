72b0133d081161624d9c97310bb9e42d086a9dd1
Luis <141833528+trainincodes@users.noreply.github.com>
2023-10-05 10:57:01 -0300

    Add files via upload

---
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,56 @@
+package com.example.composetutorial
+
+object SampleData {
+
+        val conversationSample = listOf(
+            Message(
+                "Usuário",
+                "Olá! Como posso te ajudar?"
+            ),
+            Message(
+                "Colega",
+                "Oi! Tenho uma pergunta sobre o desenvolvimento Android."
+            ),
+            Message(
+                "Usuário",
+                "Claro, ficarei feliz em ajudar. O que você precisa saber?"
+            ),
+            Message(
+                "Colega",
+                "Estou pensando na dificuldade de aprender Kotlin. O que você acha?"
+            ),
+            Message(
+                "Usuário",
+                "Kotlin é uma linguagem fantástica! Ela é concisa, expressiva e interoperável com Java. " +
+                        "Muitos desenvolvedores Android a preferem por suas características modernas e código reduzido."
+            ),
+            Message(
+                "Colega",
+                "Eu definitivamente vou tentar. Parece promissor. Alguma dica para aprender mais facilmente?"
+            ),
+            Message(
+                "Usuário",
+                "A melhor maneira de aprender é praticar. Existem muitos recursos online, " +
+                        "como tutoriais e documentação oficial. Além disso, tente criar pequenos projetos para ganhar experiência."
+            ),
+            Message(
+                "Colega",
+                "Obrigado pelas dicas! Também ouvi falar muito sobre Jetpack Compose. O que você acha disso?"
+            ),
+            Message(
+                "Usuário",
+                "Jetpack Compose é um toolkit moderno para construir interfaces de usuário nativas para Android. " +
+                        "Ele foi projetado para facilitar o desenvolvimento de interfaces de usuário. Você definitivamente deveria experimentá-lo!"
+            ),
+            Message(
+                "Colega",
+                "Parece ótimo! Vou explorar tanto o Kotlin quanto o Jetpack Compose. Obrigado pelos conselhos!"
+            ),
+            Message(
+                "Usuário",
+                "De nada! Se tiver mais alguma pergunta, sinta-se à vontade para perguntar."
+            )
+        )
+
+
+}
